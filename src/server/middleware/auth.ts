c298ceeca89aade6e75786eba3f913ea13e949df
Zoe Long <zeyulong0908@gmail.com>
2025-04-16 17:33:08 -0700

    chore: the middleware functions weird now, so I just commented out

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -1,18 +1,27 @@
-import {NextFunction, Request, Response} from "express"
+import { NextFunction, Request, Response } from "express";
+import session from "express-session";
 
-const sessionMiddleware = (
-    request: Request, 
-    response: Response, 
-    next: NextFunction
-) =>{
-    //@ts-ignore
-    if(request.session.userID !== undefined){
-        //@ts-ignore
-        response.locals.userId = request.session.userId;
-        next();
-    }else{
-        response.redirect("/auth/login");
-    }
+declare module "express-session" {
+  interface SessionData {
+    userID?: string;
+  }
 }
 
-export {sessionMiddleware};
+const sessionMiddleware = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  //@ts-ignore
+  console.log("Session ID: ", request.session.userID);
+  next();
+  //   if (request.session.userID !== undefined) {
+  //     //@ts-ignore
+  //     response.locals.userId = request.session.userId;
+  //     next();
+  //   } else {
+  //     response.redirect("/auth/login");
+  //   }
+};
+
+export { sessionMiddleware };
