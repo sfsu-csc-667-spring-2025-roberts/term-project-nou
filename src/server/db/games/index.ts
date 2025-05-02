1f20d9b4f2dc5da5c8d58e2faff131a957cf7d65
Gautier Bonhur <bonhur.gautier@gmail.com>
2025-05-02 14:44:14 +0200

    [ADD] start-game

--- src/server/db/games/index.ts
+++ src/server/db/games/index.ts
@@ -1,6 +1,14 @@
 import db from "../connection";
 import { User } from "../users";
-import { ADD_PLAYER, CONDITIONALLY_JOIN_SQL, CREATE_SQL, GET_PLAYERS_SQL } from "./sql";
+import { 
+  ADD_PLAYER, 
+  CONDITIONALLY_JOIN_SQL, 
+  CREATE_SQL, 
+  GET_PLAYERS_SQL,
+  START_GAME_SQL,
+  INITIALIZE_PLAYER_HANDS_SQL,
+  GET_GAME_STATE_SQL
+} from "./sql";
 
 const create = async (
   name: string,
@@ -38,4 +46,28 @@ const getPlayers = async (gameId: number) => {
   return await db.manyOrNone(GET_PLAYERS_SQL, { gameId });
 };
 
-export default { create, join, getPlayers };
+const startGame = async (gameId: number) => {
+  // Start a transaction to ensure all operations succeed or fail together
+  return await db.tx(async (t) => {
+    // Check if game has enough players
+    const players = await t.manyOrNone(GET_PLAYERS_SQL, { gameId });
+    const game = await t.one('SELECT min_players, max_players FROM games WHERE id = $1', [gameId]);
+    
+    if (players.length < game.min_players) {
+      throw new Error(`Game needs at least ${game.min_players} players to start`);
+    }
+
+    // Initialize game state and get the roomID
+    const { roomID } = await t.one(START_GAME_SQL, { gameId });
+    
+    // Deal initial cards to players
+    await t.none(INITIALIZE_PLAYER_HANDS_SQL, { gameId });
+    
+    // Get the complete game state using the roomID we got
+    const state = await t.one(GET_GAME_STATE_SQL, { gameId: roomID });
+    
+    return state;
+  });
+};
+
+export default { create, join, getPlayers, startGame };
